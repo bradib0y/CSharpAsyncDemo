e089c309eccd11a5afcd008a46499468121c0ace
Balazs Pukli <balazsptm@gmail.com>
2018-05-16 16:14:10 +0200

    a

--- CSharpAsyncDemo/Form1.Designer.cs
+++ CSharpAsyncDemo/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "List some integers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Do time consuming work";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(164, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(133, 33);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(133, 316);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(164, 116);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(133, 316);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(309, 444);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- CSharpAsyncDemo/Form1.cs
+++ CSharpAsyncDemo/Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace CSharpAsyncDemo
 {
     public partial class Form1 : Form
     {
+        //public delegate 
+        Random r;
         public Form1()
         {
             InitializeComponent();
+            r = new Random();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(r.Next(300));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = new ThreadStart(DoTimeConsumingWork);
+            ts += delegate { MessageBox.Show("Busy work done! ;) Message by a callback function :O"); };
+            Thread t = new Thread(ts);
+            t.Start();
+        }
+
+        private void DoTimeConsumingWork()
+        {
+            progressBar1.Invoke(new Action(delegate { progressBar1.Value = 0; }));
+            for (int i = 1; i <= 5; i++)
+            {
+                progressBar1.Invoke(new Action(delegate { progressBar1.Value += 20; }));
+                Thread.Sleep(1000);
+            }
+            listBox2.Invoke(new Action(delegate { listBox2.Items.Add("Busy work done ;)"); }));
         }
     }
 }
